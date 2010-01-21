f06e8928e9ff131e466fe2c024b0edb79a6f2a2c
Joe Wheaton <jdwfly@298179.no-reply.drupal.org>
2010-01-21 22:00:25 +0000

    #648542 by kiwi_steve:integrate with image attach.

--- colorbox.js
+++ colorbox.js
@@ -1,6 +1,15 @@
 // $Id$
 if (jQuery().colorbox) {
   Drupal.behaviors.colorbox = function (context) {
+    // Image Attach Functionality
+    $('.image-attach-body > a').each(function (i) {
+      // Modify link to jpg
+      this.href = $('.image-attach-body > a > img:eq('+i+')').attr("src").replace(".thumbnail", "");
+      // Add rel tag to group
+      this.rel = "testing1";
+      // Colorbox it
+      $("a[rel='testing1']").colorbox();
+    });
     $('.colorbox').colorbox();
   };
 }
