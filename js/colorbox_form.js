c63c5f9f7bffbcf15a84c9b33558e4b10212a131
Fredrik Jonsson <frjo@xdeb.org>
2010-04-03 08:31:50 +0000

    #760972: Allow any form to be opened in a Colorbox via a custom formatted link

---
+++ js/colorbox_form.js
@@ -0,0 +1,24 @@
+// $Id$
+Drupal.behaviors.initColorboxForm = function (context) {
+  var settings = Drupal.settings.colorbox;
+  $.urlParam = function(name, url){
+    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(url);
+    if (!results) { return 0; }
+    return results[1] || 0;
+  };
+  $('.colorbox-form', context).colorbox({
+    innerWidth:function(){
+      return $.urlParam('width', $(this).attr('href'));
+    },
+    innerHeight:function(){
+      return $.urlParam('height', $(this).attr('href'));
+    },
+    iframe:true,
+    transition:settings.transition,
+    speed:settings.speed,
+    opacity:settings.opacity,
+    maxWidth:'100%',
+    maxHeight:'100%',
+    close:settings.close
+  });
+};
