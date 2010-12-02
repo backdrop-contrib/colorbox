15c0e05c40ed2e2093d570f48b11e86d72a28c68
Fredrik Jonsson <frjo@xdeb.org>
2010-12-02 09:12:36 +0000

    Updating the stockholmsyndrome style js file to D7, missed that.

--- styles/stockholmsyndrome/colorbox_stockholmsyndrome.js
+++ styles/stockholmsyndrome/colorbox_stockholmsyndrome.js
@@ -1,19 +1,21 @@
 // $Id$
 (function ($) {
 
-Drupal.behaviors.initColorboxDefaultStyle = function (context) {
-  $(document).bind('cbox_open', function() {
-    // Hide close button initially.
-    $('#cboxClose').css('opacity', 0);
-  });
-  $(document).bind('cbox_load', function() {
-    // Hide close button. (It doesn't handle the load animation well.)
-    $('#cboxClose').css('opacity', 0);
-  });
-  $(document).bind('cbox_complete', function() {
-    // Show close button with a delay.
-    $('#cboxClose').fadeTo('fast', 0, function() {$(this).css('opacity', 1)});
-  });
+Drupal.behaviors.initColorboxStockholmsyndromeStyle = {
+  attach: function (context, settings) {
+    $(document).bind('cbox_open', function () {
+      // Hide close button initially.
+      $('#cboxClose', context).css('opacity', 0);
+    });
+    $(document).bind('cbox_load', function () {
+      // Hide close button. (It doesn't handle the load animation well.)
+      $('#cboxClose', context).css('opacity', 0);
+    });
+    $(document).bind('cbox_complete', function () {
+      // Show close button with a delay.
+      $('#cboxClose', context).fadeTo('fast', 0, function () {$(this).css('opacity', 1)});
+    });
+  }
 };
 
 })(jQuery);
