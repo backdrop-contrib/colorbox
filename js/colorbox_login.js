920ded5cf50be1ed6311892937a59d78def22845
Fredrik Jonsson <frjo@xdeb.org>
2010-12-03 08:58:34 +0000

    #987794: Remove fixed width/height for Login-block/form. Instead add initial width/height so it looks nicer when opening.

--- js/colorbox_login.js
+++ js/colorbox_login.js
@@ -6,8 +6,8 @@ Drupal.behaviors.initColorboxLogin = {
     $("a[href*='/user/login'], a[href*='?q=user/login']", context).once('init-colorbox-login-processed', function () {
       this.href = this.href.replace(/user\/login/,"user/login/colorbox");
     }).colorbox({
-      innerWidth:250,
-      innerHeight:230,
+      initialWidth:200,
+      initialHeight:200,
       onComplete:function(){
         $('#edit-name').focus();
       }
