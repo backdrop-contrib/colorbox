8ca4da91d6b612d1598a6be3817b6a7f61cc6037
Fredrik Jonsson <frjo@xdeb.org>
2011-03-23 07:58:50 +0100

    Improved the Colorbox user login feature.

--- js/colorbox_login.js
+++ js/colorbox_login.js
@@ -6,7 +6,17 @@ Drupal.behaviors.initColorboxLogin = {
       return;
     }
     $("a[href*='/user/login'], a[href*='?q=user/login']", context).once('init-colorbox-login-processed', function () {
-      this.href = this.href.replace(/user\/login/,"user/login/colorbox");
+      var path = this.href;
+      var new_path = path.replace(/user\/login/,'user/login/colorbox')
+      var addquery = (path.indexOf('?') !=-1) ? '&' : '?';
+
+      // If no destination, add one to the current page.
+      if (path.indexOf('destination') !=-1) {
+        this.href = new_path;
+      }
+      else {
+        this.href = new_path + addquery + 'destination=' + window.location.pathname.substr(1);
+      }
     }).colorbox({
       initialWidth:200,
       initialHeight:200,
