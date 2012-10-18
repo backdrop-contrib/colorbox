54e84607eb8995785f3cf629b8e23910c8089d61
Fredrik Jonsson <frjo@xdeb.org>
2012-10-18 14:49:37 +0200

    Improved the look of the close button in the new plain style, part 3.

--- styles/plain/colorbox_style.js
+++ styles/plain/colorbox_style.js
@@ -5,8 +5,8 @@ Drupal.behaviors.initColorboxPlainStyle = {
     $(document).bind('cbox_complete', function () {
       // Make all the controls invisible.
       $('#cboxCurrent, #cboxSlideshow, #cboxPrevious, #cboxNext', context).addClass('element-invisible');
-      // Replace "Close" with "×".
-      $('#cboxClose', context).html('\327');
+      // Replace "Close" with "×" and show.
+      $('#cboxClose', context).html('\327').css('visibility', 'visible');
       // Only run if there is a title.
       if ($('#cboxTitle:empty', context).length == false) {
         $('#cboxLoadedContent img', context).bind('mouseover', function () {
