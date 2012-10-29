45241969f4455dbf1b4ec320c80e3ba0500b75ec
Fredrik Jonsson <frjo@xdeb.org>
2012-10-29 13:44:01 +0100

    Fix so animation in the plain style do not queue, CSS rounded corners on cboxContent to avoid flashing scuare corners on loading/next/prev.

--- styles/plain/colorbox_style.js
+++ styles/plain/colorbox_style.js
@@ -5,28 +5,24 @@ Drupal.behaviors.initColorboxPlainStyle = {
     $(document).bind('cbox_complete', function () {
       // Make all the controls invisible.
       $('#cboxCurrent, #cboxSlideshow, #cboxPrevious, #cboxNext', context).addClass('element-invisible');
-      // Set some vars.
-      var cboxTitle = $('#cboxTitle', context);
-      var cboxClose = $('#cboxClose', context);
       // Replace "Close" with "×" and show.
-      cboxClose.html('\327').addClass('cbox-close-plain');
-      // Only run if there is a title.
-      var isTitle = ($('#cboxTitle:empty', context).length == false) ? true : false;
-      $('#cboxLoadedContent img', context).bind('mouseover', function () {
-        if (isTitle == true) {
-          cboxTitle.slideDown();
+      $('#cboxClose', context).html('\327').addClass('cbox-close-plain');
+      // Hide empty title.
+      if ($('#cboxTitle:empty', context).length == true) {
+        $('#cboxTitle', context).hide();
+      }
+      $('#cboxLoadedContent', context).bind('mouseover', function () {
+        $('#cboxClose', context).animate({opacity: 1}, {queue: false, duration: "fast"});
+        if ($('#cboxTitle:empty', context).length == false) {
+          $('#cboxTitle', context).slideDown();
         }
-        cboxClose.animate({opacity: 1}, 'fast');
       });
       $('#cboxOverlay', context).bind('mouseover', function () {
-        if (isTitle == true) {
-          cboxTitle.slideUp();
+        $('#cboxClose', context).animate({opacity: 0}, {queue: false, duration: "fast"});
+        if ($('#cboxTitle:empty', context).length == false) {
+          $('#cboxTitle', context).slideUp();
         }
-        cboxClose.animate({opacity: 0}, 'fast');
       });
-      if (isTitle == false) {
-        cboxTitle.hide();
-      }
     });
     $(document).bind('cbox_closed', function () {
       $('#cboxClose', context).removeClass('cbox-close-plain');
